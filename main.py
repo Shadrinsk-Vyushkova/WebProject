84c4036a9918bba66ae830915bd8b88df4164f4a
Shadrinsk-Vyushkova <ragity@yandex.ru>
2021-04-18 10:06:42 +0500

    change main to index

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    pass
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('base.html', title="Начало")
 
 
 if __name__ == '__main__':
-    main()
+    app.run(port=8080, host='127.0.0.1')
