c2cf2ac8204129e24a339f6c82fe1dc1068f71d7
Shadrinsk-Vyushkova <ragity@yandex.ru>
2021-04-18 11:08:31 +0500

    change post from form

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'WebProject_secret_key'
 
 @app.route('/')
-@app.route('/index')
 def index():
-    return render_template('base.html', title="Начало")
+    form = LoginForm()
+    return render_template('index.html', title="Начало", form=form)
+
+@app.route('/start', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        name = request.form.get('username')
+    return render_template('start_test.html', title="Начало тестирования", username=name)
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Имя:')
+    submit = SubmitField('Начать')
 
 
 if __name__ == '__main__':
